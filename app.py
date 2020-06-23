fa0a5c371247e7f6e029f4b2941754705c668343
wataru okada <wataru.okada@flect.co.jp>
2020-06-24 00:06:30 +0900

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 
     port = int(os.environ.get("PORT", 38888))
 
-    heroku = False
+    heroku = True
     if heroku == True:
         app.run(host='0.0.0.0', port=port)
     else:
